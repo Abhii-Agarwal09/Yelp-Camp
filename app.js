9a0541ac9cce72d900c94cbcc39c065fabb0c15b
Abhii-Agarwal09 <abhishek.agarwal.work20@gmail.com>
2021-07-10 16:46:50 +0530

    reviews functionality added

--- app.js
+++ app.js
@@ -4,11 +4,12 @@ const path = require('path');
 const app = express();
 const ejsMate = require('ejs-mate');
 const Joi = require('joi');
-const {campgroundSchema} = require('./schemas');
+const { campgroundSchema, reviewSchema } = require('./schemas');
 const methodOverride = require('method-override')
 const Campground = require('./models/campground');
 const catchAsync = require('./utils/catchAsync');
 const expressError = require('./utils/expressError');
+const Review = require('./models/review');
 
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
@@ -17,8 +18,19 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
 const validateCampground = (req, res, next) => {
-    const error = campgroundSchema.validate(req.body);
-    if(error) {
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new expressError(msg, 400)
+    }
+    else {
+        next()
+    }
+}
+
+const validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body);
+    if (error) {
         const msg = error.details.map(el => el.message).join(',');
         throw new expressError(msg, 400)
     }
@@ -61,7 +73,7 @@ app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) =
 }))
 
 app.get('/campgrounds/:id', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
+    const campground = await Campground.findById(req.params.id).populate('reviews');
     res.render('campgrounds/show', { campground });
 }));
 
@@ -69,6 +81,22 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
+}))
+
+app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+}))
+
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
+    const {id, reviewId} = req.params;
+    await Campground.findByIdAndUpdate(id,{$pull: {reviews: reviewId}});
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
 }))
 
 app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
@@ -87,9 +115,9 @@ app.all('*', (req, res, next) => {
 })
 
 app.use((err, req, res, next) => {
-    const { statusCode = 500} = err;
-    if(!err.message) err.message = 'Something went wrong!'
-    res.status(statusCode).render('error', {err});
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Something went wrong!'
+    res.status(statusCode).render('error', { err });
 })
 app.listen(3000, () => {
     console.log('APP IS LISTENING ON PORT 3000');
