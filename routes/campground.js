7555bea18e05cd66046d9ea0bb0148a9895dcd41
Abhii-Agarwal09 <abhishek.agarwal.work20@gmail.com>
2021-07-18 21:19:43 +0530

    routing bug fixed and star rating added

--- routes/campground.js
+++ routes/campground.js
@@ -2,23 +2,21 @@ const express = require('express');
 const router = express.Router();
 
 const catchAsync = require('../utils/catchAsync');
-const expressError = require('../utils/expressError');
 const Campground = require('../models/campground');
-const { campgroundSchema } = require('../schemas');
 const { isLoggedIn, validateCampground, isAuthor } = require('../middleware');
 const campgrounds = require('../controllers/campgrounds');
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
 
-router.get('/new', isLoggedIn, campgrounds.renderNewform)
+router.get('/new', isLoggedIn, campgrounds.renderNewForm)
 
 router.route('/:id')
     .get(catchAsync(campgrounds.showCampground))
     .put(isLoggedIn, isAuthor, catchAsync(campgrounds.editCampground))
-    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
+    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground))
 
-router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditCampForm));
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditCampForm))
 
 module.exports = router;
