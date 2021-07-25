772d2bb62ef88547bca9e1a7a8ea580b27b6790c
Abhii-Agarwal09 <abhishek.agarwal.work20@gmail.com>
2021-07-25 13:07:21 +0530

    code refactored

--- app.js
+++ app.js
@@ -3,28 +3,40 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 const express = require('express');
-const mongoose = require('mongoose');
 const path = require('path');
-const app = express();
+const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
-const Joi = require('joi');
-const methodOverride = require('method-override')
 const session = require('express-session');
-const expressError = require('./utils/expressError');
 const flash = require('connect-flash');
+const expressError = require('./utils/expressError');
+const methodOverride = require('method-override')
 const passport = require('passport');
 const localStrategy = require('passport-local');
 const User = require('./models/user');
-const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
-const secret = process.env.SECRET || 'thisshouldbeagreatsecret';
-
-const MongoStore = require('connect-mongo')(session);
-
+const mongoSanitize = require('express-mongo-sanitize');
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campground');
 const reviewRoutes = require('./routes/review');
+
+const MongoStore = require('connect-mongo')(session);
+
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+// 'mongodb://localhost:27017/yelp-camp'
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+})
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("database connected");
+})
+
+const app = express();
 
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
@@ -33,8 +45,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(mongoSanitize());
-app.use(helmet({ contentSecurityPolicy: false }));
+app.use(mongoSanitize({
+    replaceWith: '_'
+}));
+
+const secret = process.env.SECRET || 'thisshouldbeagreatsecret';
 
 const store = new MongoStore({
     url: dbUrl,
@@ -60,15 +75,12 @@ const sessionConfig = {
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
 }
+
 app.use(session(sessionConfig));
+app.use(helmet());
 app.use(flash());
+const Joi = require('joi');
 
-app.use(passport.initialize());
-app.use(passport.session());
-passport.use(new localStrategy(User.authenticate()));
-
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
 
 const scriptSrcUrls = [
     "https://stackpath.bootstrapcdn.com/",
@@ -92,6 +104,7 @@ const connectSrcUrls = [
     "https://b.tiles.mapbox.com/",
     "https://events.mapbox.com/",
 ];
+
 const fontSrcUrls = [];
 app.use(
     helmet.contentSecurityPolicy({
@@ -114,19 +127,13 @@ app.use(
     })
 );
 
-// 'mongodb://localhost:27017/yelp-camp'
-mongoose.connect(dbUrl, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
-})
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
 
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
-    console.log("database connected");
-})
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 
 app.use((req, res, next) => {
     res.locals.currentUser = req.user;
@@ -153,7 +160,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err });
 })
 
-const port = process.env.PORT || 3000; 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Serving on port ${port}`);
 })
