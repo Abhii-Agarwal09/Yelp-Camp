573741303bbdc5d40f3fb190934b348df1b26e91
Abhii-Agarwal09 <abhishek.agarwal.work20@gmail.com>
2021-07-25 12:50:09 +0530

    try resolving deployment issue 2

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const helmet = require('helmet');
 const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 const secret = process.env.SECRET || 'thisshouldbeagreatsecret';
 
-const MongoStore = require('connect-mongo');
+const MongoStore = require('connect-mongo')(session);
 
 const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campground');
