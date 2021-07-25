0b7e3c12fe0e96249168011fdecd983e5467dfcb
Abhii-Agarwal09 <abhishek.agarwal.work20@gmail.com>
2021-07-25 15:01:39 +0530

    cloudinary name corrected in content security policy

--- app.js
+++ app.js
@@ -126,7 +126,7 @@ app.use(
                 "'self'",
                 "blob:",
                 "data:",
-                "https://res.cloudinary.com/douqbebwk/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://res.cloudinary.com/dn81jz8tt/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
                 "https://images.unsplash.com/",
             ],
             fontSrc: ["'self'", ...fontSrcUrls],
